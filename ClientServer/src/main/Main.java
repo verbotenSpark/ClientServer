90dda08430db1ffa5c83fa0f6c2ed0e3cc9362b9
verbotenSpark <utkarshmishra6676+github@gmail.com>
2025-09-06 15:22:24 +0530

    tested gitbash functionality

--- ClientServer/src/main/Main.java
+++ ClientServer/src/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 import javax.swing.JFrame;
 public class Main {
-	//meow meow edit
+	//gitbash test
 	public static void main(String[] args) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
